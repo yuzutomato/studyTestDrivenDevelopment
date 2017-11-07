b4ac6109cea62b903e45557867745e44bcbd154e
yuzutomato <kousanden@gmail.com>
2017-11-07 17:23:52 +0900

    TODO「$5 * 2 = $10」を消化。ソースコードとテストコードの重複を排除。

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,15 +2,14 @@ package money;
 
 public class Dollar {
 
-	int amount = 10;
+	int amount;
 
 	public Dollar(int amount) {
-		// TODO 自動生成されたコンストラクター・スタブ
+		this.amount = amount;
 	}
 
 	public void times(int multiplier) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		amount *= multiplier;
 	}
 
 }
