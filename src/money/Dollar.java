71e71904712b0c5f1dc6083a790cdb874a9ecdd5
yuzutomato <kousanden@gmail.com>
2017-11-07 17:01:16 +0900

    初期コミット

---
+++ src/money/Dollar.java
@@ -0,0 +1,16 @@
+package money;
+
+public class Dollar {
+
+	int amount = 10;
+
+	public Dollar(int amount) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public void times(int multiplier) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
