660b4f32f6f915c13227964c5adbf42f529e41c6
yuzutomato <kousanden@gmail.com>
2017-11-09 22:27:00 +0900

    amountをMoneyクラスに引き上げ

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,8 +2,6 @@ package money;
 
 public class Dollar extends Money {
 
-	private int amount;
-
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
--- src/money/Money.java
+++ src/money/Money.java
@@ -1,5 +1,5 @@
 package money;
 
 public class Money {
-
+	protected int amount;
 }
