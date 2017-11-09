f3416f19258ee7c6e70520e5fe4b701601ff89f1
yuzutomato <kousanden@gmail.com>
2017-11-09 22:34:43 +0900

    書かれているべきだったFrancクラスの比較テストを追加

--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -17,6 +17,9 @@ class MoneyTest {
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6))); // 2つめの実例
+
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
 	}
 
 	@Test
