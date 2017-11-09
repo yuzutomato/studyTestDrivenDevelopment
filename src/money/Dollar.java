f4f800f76296e780fe6869dab874f02411d925ee
yuzutomato <kousanden@gmail.com>
2017-11-09 22:29:39 +0900

    equalsメソッドを引き上げ

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -9,10 +9,4 @@ public class Dollar extends Money {
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
-	}
-
 }
