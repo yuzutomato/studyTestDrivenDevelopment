e4532535fde7b58c3de9ee2631ccec65929d7b1f
yuzutomato <kousanden@gmail.com>
2017-11-10 22:21:36 +0900

    Moneyクラスでdollarが作成できるよう、Factoryメソッドを想定したテストを追加（テストは失敗する）

--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -8,7 +8,7 @@ class MoneyTest {
 
 	@Test
 	void testMultiplication() {
-		Dollar five = new Dollar(5);
+		Dollar five = new Money.doller(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
