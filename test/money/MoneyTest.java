a2ec7cee3b7401033dde2812c228f9627aab61db
yuzutomato <kousanden@gmail.com>
2017-11-10 22:04:26 +0900

    DollarとFrancを比較するテストを追加

--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -20,6 +20,8 @@ class MoneyTest {
 
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+
+		assertFalse(new Franc(5).equals(new Dollar(5)));
 	}
 
 	@Test
