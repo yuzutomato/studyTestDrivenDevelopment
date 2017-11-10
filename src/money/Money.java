de2da7c9bca24b736bf6dc49199128c5bc83179a
yuzutomato <kousanden@gmail.com>
2017-11-10 22:26:53 +0900

    dollarメソッドの戻り値を変更

--- src/money/Money.java
+++ src/money/Money.java
@@ -11,7 +11,7 @@ public abstract class Money {
 
 	abstract Money times(int multiplier);
 
-	static Dollar dollar(int amount) {
+	static Money dollar(int amount) {
 		return new Dollar(amount);
 	}
 }
