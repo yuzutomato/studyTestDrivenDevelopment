0fbb2c5da00b14171798ca7ce4f63de51ec3bf8f
yuzutomato <kousanden@gmail.com>
2017-11-11 18:51:18 +0900

    Francクラスを参照しているテストを削除し、Francクラスも削除

--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -36,9 +36,4 @@ class MoneyTest {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
-
-	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
 }
