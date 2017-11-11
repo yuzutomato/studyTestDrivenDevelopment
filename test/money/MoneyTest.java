803a8971fa02895e2554128c68644ca688bab567
yuzutomato <kousanden@gmail.com>
2017-11-11 18:52:23 +0900

    冗長なテストを削除

--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -21,16 +21,6 @@ class MoneyTest {
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
-	/*
-	 * 冗長なテストになった？
-	 */
-	@Test
-	void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-
 	@Test
 	public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
