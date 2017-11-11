c746d52594a49e665d31209440977554aad8fffd
yuzutomato <kousanden@gmail.com>
2017-11-11 22:58:07 +0900

    reduceメソッドの中身をSumクラスに移し、フィールドへのアクセスを排除

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -3,7 +3,6 @@ package money;
 public class Bank {
 	public Money reduce(Expression source, String to) {
 		Sum sum = (Sum) source;
-		int amount = sum.augend.amount + sum.added.amount;
-		return new Money(amount, to);
+		return sum.reduce(to);
 	}
 }
--- src/money/Sum.java
+++ src/money/Sum.java
@@ -8,4 +8,9 @@ public class Sum implements Expression {
 		this.augend = augend;
 		this.added = added;
 	}
+
+	Money reduce(String to) {
+		int amount = augend.amount + added.amount;
+		return new Money(amount, to);
+	}
 }
