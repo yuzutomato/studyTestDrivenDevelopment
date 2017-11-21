6b65abafba56e2420c2db08b39d3e93ef7a1132d
yuzutomato <kousanden@gmail.com>
2017-11-21 23:10:28 +0900

    べた書きのレート実装を、マップから検索して返すよう実装

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -1,15 +1,20 @@
 package money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+	private Map<Pair, Integer> rates = new HashMap<>();
 	public Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 
 	public void addRate(String from, String to, int rate) {
-
+		rates.put(new Pair(from, to), rate);
 	}
 
 	int rate(String from, String to) {
-		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+		if (from.equals(to)) return 1;
+		return rates.get(new Pair(from, to));
 	}
 }
