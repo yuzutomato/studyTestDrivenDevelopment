982b5b2d7e7cc94455b874d0af593dcf9f746df9
yuzutomato <kousanden@gmail.com>
2017-11-24 21:47:53 +0900

    テストコードのMoney型をExpression型にし、コンパイルエラーを解消

--- src/money/Expression.java
+++ src/money/Expression.java
@@ -2,4 +2,5 @@ package money;
 
 public interface Expression {
 	public Money reduce(Bank bank, String to);
+	public Expression plus(Expression addend);
 }
--- src/money/Sum.java
+++ src/money/Sum.java
@@ -9,6 +9,10 @@ public class Sum implements Expression {
 		this.addend = added;
 	}
 
+	public Expression plus(Expression addend) {
+		return null;
+	}
+
 	public Money reduce(Bank bank, String to) {
 		int amount = augend.reduce(bank, to).amount
 				+ addend.reduce(bank, to).amount;
